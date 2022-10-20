5352347e5cf3df42f5cc964f95811b8263d7561a
RAMENARD <raphael.menard@ynov.com>
2022-10-21 01:37:37 +0200

    peaufinage et ajout de ping, il est temps d'aller dormir

--- hope.py
+++ hope.py
@@ -7,16 +7,20 @@ def main():
         ipChoose = input("Rentrez votre addresse IP avec son masque (ex: xxx.xxx.xxx.xxx/xx)\n")
         if valableIp(ipChoose):
             tabIp = scanARP(ipChoose)
+        inputUser = input("Voulez vous ping les differents IP ?[Y/N] ").lower()
+        if inputUser == "y" or inputUser == "yes":
+            scanIP(tabIp)
+        
     else:    
         for i in range(1, len(sys.argv)):
             if valableIp(sys.argv[i]):
                 tabIp = scanARP(sys.argv[i])
-        inputUser = input("Voulez vous ping les differents IP ?[Y/N]").lower()
-        if inputUser == "y":
+        inputUser = input("Voulez vous ping les differents IP ?[Y/N] ").lower()
+        if inputUser == "y" or inputUser == "yes":
             scanIP(tabIp)
     
-    print(tabIp)
-    print(sys.argv[1:])
+#    print(tabIp)
+#    print(sys.argv[1:])
 
 def valableIp(testIp):
     regex = re.search("^([01]?\d\d?|2[0-4]\d|25[0-5])(?:\.[01]?\d\d?|\.2[0-4]\d|\.25[0-5]){3}(?:/[1-2]?\d|/3[0-2])$", testIp)
@@ -29,14 +33,14 @@ def valableIp(testIp):
 def scanARP(ipTest):
     tabIp = []
     tabMac = []
-    ipGood = ipTest
-    print("Le scan est en cours")
-    testARP = srp(Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst=ipGood), timeout=2, verbose=False)[0]
+    testARP = srp(Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst=ipTest), timeout=2, verbose=False)[0]
     for i in range(0, len(testARP)):
         tabIp.append(testARP[i][1].psrc)
         tabMac.append(testARP[i][1].hwsrc)
         with open("rapport.txt", "a") as file:
             file.write("ip : " + tabIp[i] + " mac : " + tabMac[i] + '\n')
+    with open("rapport.txt", "a") as file:
+        file.write("------------------------------------------------------------------------------" +  '\n')
     print("Le scan est terminé \n")
     return tabIp
 
@@ -59,7 +63,7 @@ def scanIP(tabIp):
         file.write("ces IP appartiennent surement à un linux ou a un macOs : " + linux + '\n')
         file.write("ces IP appartiennent surement à un windows : " + win + '\n')
         file.write("------------------------------------------------------------------------------" +  '\n')
-    print(tabICMP)
+#    print(tabICMP)
 #    return tabICMP
 
 main()
