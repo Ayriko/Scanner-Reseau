261ff70342b680fce160ea480e5e2265c571496b
RAMENARD <raphael.menard@ynov.com>
2022-10-21 00:00:56 +0200

    on passe aux IP trop coolos

--- hope.py
+++ hope.py
@@ -1,20 +1,21 @@
 import sys
 import re
-from scapy.all import ARP, Ether, srp
+from scapy.all import ARP, Ether, srp, sr, IP, ICMP
 
 def main():
     if sys.argv[1:] == [] :
         ipChoose = input("Rentrez votre addresse IP avec son masque (ex: xxx.xxx.xxx.xxx/xx)\n")
         if valableIp(ipChoose):
-            scanARP(ipChoose)
+            tabIp = scanARP(ipChoose)
+    else:    
         for i in range(1, len(sys.argv)):
             if valableIp(sys.argv[i]):
-                scanARP(sys.argv[i])    
-            if "non":
-                print("sadge")
-            if "nique":
-                print("fonce")
-
+                tabIp = scanARP(sys.argv[i])
+                inputUser = input("Voulez vous ping les differents ip ?[Y/N]")
+                if inputUser.toLower() == "y":
+                    scanIP(tabIp)
+    
+    print(tabIp)
     print(sys.argv[1:])
 
 def valableIp(testIp):
@@ -23,17 +24,28 @@ def valableIp(testIp):
         return True
 
     print("l'ip n'est pas valide")
-    main()
+    
 
 def scanARP(ipTest):
     tabIp = []
     tabMac = []
     ipGood = ipTest
+    print("Le scan est en cours")
     testARP = srp(Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst=ipGood), timeout=2, verbose=False)[0]
     for i in range(0, len(testARP)):
         tabIp.append(testARP[i][1].psrc)
         tabMac.append(testARP[i][1].hwsrc)
         with open("rapport.txt", "a") as file:
             file.write("ip : " + tabIp[i] + " mac : " + tabMac[i] + '\n')
+    with open("rapport.txt", "a") as file:
+        file.write("------------------------------------------------------------------------------" +  '\n')
+    print("Le scan est terminé \n")
+    return tabIp
+
+def scanIP(tabIp):
+    for i in range(0, len(tabIp)):
+        ansIP = sr(IP(dst=tabIp[i])/ICMP(), timeout=1, verbose=False)[0]
+        ansIP.summary(lambda s, r: r.sprintf("%IP.src% is alive"))
+        print(IP.src)
 
 main()
