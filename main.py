bbd49d8e5b0d013cac0d30cb77a18fdaee67bd28
RAMENARD <raphael.menard@ynov.com>
2022-10-20 22:26:15 +0200

    debut de l'espoir

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
-from datetime import datetime
-from scapy.all import ICMP, IP, send, sr
-
-# # sr au lieu de send permet de stocker et aussi de gérer les erreurs
-# toto = IP(dst="10.3.1.12")/ICMP()
-# sr(toto)
-# toto.show()
+import re
+from scapy.all import ARP, Ether, srp
 
 def main():
-    with open("ip_list.txt", "a") as file:
-        file.write(datetime.today + '\n')
-    for i in range (10,20):
-        ipGood = IP(dst="10.3.1." + i)/ICMP()
-        if ipGood:
+    tabIp = []
+    tabMac = []
+    ipChoose = input("Rentrez votre addresse IP avec son masque (ex: xxx.xxx.xxx.xxx/xx)\n")
+    regex = re.search("^(((([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])(((\/([4-9]|[12][0-9]|3[0-2]))?)|\s?-\s?((([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]))))(,\s?|$))+", ipChoose)
+    if regex:
+        testARP, unansARP = srp(Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst=ipChoose), timeout=2, verbose=False)
+        testARP.summary(lambda s,r: r.sprintf("%Ether.src% %ARP.psrc%") )
+        for i in range(0,len(testARP)):
+            tabIp.append(testARP[i][1].psrc)
+            tabMac.append(testARP[i][1].hwsrc)
             with open("ip_list.txt", "a") as file:
-                file.write(ipGood + '\n')
+                file.write("ip :  "+ tabIp[i] + " mac : "  + tabMac[i] + '\n')
+    else:
+        main()
 
 
-# scapy.send(scapy.IP()/scapy.ICMP(id=1, seq=1))
+main()
